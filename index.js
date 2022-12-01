6aec44403b53b79636aca2b098b4ce4f845cf9f4
ngachadavid <davidngacha01@gmail.com>
2022-12-01 15:21:49 +0300

    Done with assignments

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(name, events) {
+    let thankYouCards = [];
+    for (let i = 0; i < name.length; i++) {
+        thankYouCards.push( `Thank you, ${name[i]}, for the wonderful ${events} gift!`);
+    }
+    return thankYouCards;
+}
+
+function countDown(num) {
+    while(num > 0) {
+        console.log(num);
+        num -=1;
+    }
+    console.log(num);
+}
